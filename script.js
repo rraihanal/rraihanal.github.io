318b2d283bc275836850bb7e45fb3cd02a8a29bd
rraihanal <alghaufarir21@gmail.com>
2022-06-28 04:51:20 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let btnPertama = document.querySelector('#pertama');
+let btnKedua = document.querySelector('#kedua');
+let btnKetiga = document.querySelector('#ketiga');
+let btnKeempat = document.querySelector('#keempat');
+let btnKelima = document.querySelector('#kelima');
+let btnKeenam = document.querySelector('#keenam');
+let btnKetujuh = document.querySelector('#ketujuh');
+let btnKedelapan = document.querySelector('#kedelapan');
+let btnKesembilan = document.querySelector('#kesembilan');
+let btnKesepuluh = document.querySelector('#kesepuluh');
+let btnKesebelas = document.querySelector('#kesebelas');
+let btnKeduabelas = document.querySelector('#keduabelas');
+let btnKetigabelas = document.querySelector('#ketigabelas');
+let btnKeempatbelas = document.querySelector('#keempatbelas');
+let btnKelimabelas = document.querySelector('#kelimabelas');
+let btnKeenambelas = document.querySelector('#keenambelas');
+
+
+btnPertama.addEventListener('click', () => btnPertama.style.backgroundColor = '#2A2727')
+btnKedua.addEventListener('click', () => btnKedua.style.backgroundColor = '#2A2727')
+btnKetiga.addEventListener('click', () => btnKetiga.style.backgroundColor = '#2A2727')
+btnKeempat.addEventListener('click', () => btnKeempat.style.backgroundColor = '#2A2727')
+btnKelima.addEventListener('click', () => btnKelima.style.backgroundColor = '#2A2727')
+btnKeenam.addEventListener('click', () => btnKeenam.style.backgroundColor = '#2A2727')
+btnKetujuh.addEventListener('click', () => btnKetujuh.style.backgroundColor = '#2A2727')
+btnKedelapan.addEventListener('click', () => btnKedelapan.style.backgroundColor = '#2A2727')
+btnKesembilan.addEventListener('click', () => btnKesembilan.style.backgroundColor = '#2A2727')
+btnKesepuluh.addEventListener('click', () => btnKesepuluh.style.backgroundColor = '#2A2727')
+btnKesebelas.addEventListener('click', () => btnKesebelas.style.backgroundColor = '#2A2727')
+btnKeduabelas.addEventListener('click', () => btnKeduabelas.style.backgroundColor = '#2A2727')
+btnKetigabelas.addEventListener('click', () => btnKetigabelas.style.backgroundColor = '#2A2727')
+btnKeempatbelas.addEventListener('click', () => btnKeempatbelas.style.backgroundColor = '#2A2727')
+btnKelimabelas.addEventListener('click', () => btnKelimabelas.style.backgroundColor = '#2A2727')
+btnKeenambelas.addEventListener('click', () => btnKeenambelas.style.backgroundColor = '#2A2727')
